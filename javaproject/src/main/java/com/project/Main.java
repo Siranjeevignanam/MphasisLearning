7c906f77762e0c679e01dc333fdff9bfe3671ac5
Siranjeevi Gnanam <siranjeevignanam73@gmail.com>
2025-11-27 17:34:21 +0530

    Add files via upload

---
+++ javaproject/src/main/java/com/project/Main.java
@@ -0,0 +1,33 @@
+package com.project;
+
+import java.util.Scanner;
+
+import com.project.property.PropertyTax;
+import com.project.total.Total;
+import com.project.vehicle.VehicleTax;
+
+public class Main {
+public static void main(String[] args) throws Exception {
+	
+	VehicleTax v=new VehicleTax();
+	PropertyTax p=new PropertyTax();
+	Total t=new Total();
+	
+	while(true)
+	{
+		
+		System.out.println("Enter 1 for Property tax \nEnter 2 for Vehicle tax \nEnter 3 for Total \nEnter 4 for Exit  ");
+		Scanner sc=new Scanner(System.in);
+		int ch=sc.nextInt();
+		switch(ch)
+		{
+		case 1: p.method();break;
+		case 2: v.method();break;
+		case 3:t.totalTax();break;
+		case 4 : System.out.println("Thank you for using");break;
+		default:System.out.println("Wrong choice");
+		}
+	}
+	
+}
+}
---
+++ javaproject/src/main/java/com/project/Tax.java
@@ -0,0 +1,6 @@
+package com.project;
+
+public interface Tax {
+public double totalTax();
+public int totalRows();
+}
