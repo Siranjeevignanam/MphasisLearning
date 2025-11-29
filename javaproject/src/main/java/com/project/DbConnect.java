8032985117589fa825ddf351db0afb48fafa44e1
Siranjeevi Gnanam <siranjeevignanam73@gmail.com>
2025-11-29 17:15:30 +0530

    Add files via upload

---
+++ javaproject/src/main/java/com/project/DbConnect.java
@@ -0,0 +1,20 @@
+package com.project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbConnect {
+
+	public static Connection c=null;
+	public static Connection getConnection() throws Exception
+	{
+		
+		if(c==null)
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			c=DriverManager.getConnection("jdbc:mysql://localhost:3306/javaproject","root","root@39");
+		}
+		return c;
+		
+	}
+}
