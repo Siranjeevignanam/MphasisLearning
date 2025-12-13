4a32b04ae4d15a27816d3aa120be166c6c6fc41f
Siranjeevi Gnanam <siranjeevignanam73@gmail.com>
2025-12-13 17:54:49 +0530

    Add files via upload

---
+++ src/main/java/project/controller/AdminLogin.java
@@ -0,0 +1,46 @@
+package project.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import project.dao.AdminDao;
+import project.model.Admin;
+
+
+
+
+@WebServlet("/AdminServlet")
+public class AdminLogin extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        Admin admin = new Admin();
+        Admin validAdmin = null;
+
+        
+        admin.setName(request.getParameter("name")); 
+        admin.setPassword(request.getParameter("password"));
+
+        AdminDao dao = new AdminDao();
+        validAdmin = dao.validate(admin);
+
+        if (validAdmin != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("admin", validAdmin);
+            response.sendRedirect(request.getContextPath() + "/dashboard.jsp"); 
+        } else {
+        	request.setAttribute("msg", "Invalid Credentials.");
+        	RequestDispatcher rd = request.getRequestDispatcher("Adminlogin.jsp");
+        	rd.forward(request, response);
+
+        }
+    }
+}
---
+++ src/main/webapp/userdashboard.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>User dash board</title>
+</head>
+<body>
+
+<button>Take quiz</button><br><br>
+<button>Leaderboard</button><br><br>
+<button>Logout</button>
+
+</body>
+</html>
